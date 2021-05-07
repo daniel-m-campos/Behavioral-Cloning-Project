a1e2a4732560cea96bc6a5359815e23e5d3c9931
Daniel M Campos <daniel.m.campos@icloud.com>
2021-05-07 11:05:49 -0500

    Add virtualenv support

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
 requirements_file = open("test_requirements.txt")
+test_requirements_file = open("test_requirements.txt")
 
 setup(
-    name="lane_finder",
+    name="behavior_cloning",
     description="Solutions for CarND Behavioral Cloning project",
     url="https://github.com/daniel-m-campos/Behavioral-Cloning-Project",
     author="Daniel Campos",
     version="0.0.1",
     package_dir={"": "src"},
     packages=find_packages("src"),
-    extras_require=requirements_file.read().strip().split("\n"),
+    install_requires=requirements_file.read().strip().split("\n"),
+    extras_require=test_requirements_file.read().strip().split("\n"),
 )
