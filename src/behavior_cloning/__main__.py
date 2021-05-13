f79b9ad3a781724ef7a27ecc6791d4aacd927f52
Daniel M Campos <daniel.m.campos@icloud.com>
2021-05-12 19:45:01 -0500

    Change data directories

--- src/behavior_cloning/__main__.py
+++ src/behavior_cloning/__main__.py
@@ -2,22 +2,23 @@ from pathlib import Path
 
 from tensorflow import keras
 
-import data
-from model import train
+from behavior_cloning import data
+from behavior_cloning.model import train
+
+
+def plot_model(path: Path):
+    model = keras.models.load_model(path / "model.h5")
+    keras.utils.plot_model(model, to_file="../../img/model2.png", show_shapes=True)
 
 
 def main(path: Path):
     trainer, validator = data.create_validation_generators(
         path, validation_split=0.2, shift=0.125, batch_size=100
     )
-    model = train(trainer, validator, epochs=2)
+    model = train(trainer, validator, epochs=10)
     model.save(path / "model.h5")
-
-
-def plot_model(path: Path):
-    model = keras.models.load_model(path / "model.h5")
-    keras.utils.plot_model(model, to_file="../../img/model.png", show_shapes=True)
+    plot_model(path / "model.png")
 
 
 if __name__ == "__main__":
-    main(Path("/home/dcampos/Documents/CarND/data"))
+    main(Path("../../data/track2"))
